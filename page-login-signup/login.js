c8e30ec83ea01e6f4724d2ccdb12535e761008e3
nhannm-vn <nhanmin911@gmail.com>
2024-09-30 17:32:03 +0700

    30-9-2024

---
+++ page-login-signup/login.js
@@ -0,0 +1,18 @@
+//giúp chuyển trang mượt
+//**Giúp cho trang web chuyển mượt hơn
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 1000)
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+});
+
+
+
+
+
+
